a2cdbf3fa9bc1097364cfa252e74b65d4e935416
Yanuo Ma <yanuoma@microsoft.com>
2025-11-20 23:37:39 -0500

    fix: use vim.fn.exepath instead of system call for node path resolution

--- lua/nvim-eslint/settings.lua
+++ lua/nvim-eslint/settings.lua
@@ -42,13 +42,9 @@ function M.use_flat_config(bufnr)
 end
 
 function M.resolve_node_path()
-  local is_windows = vim.loop.os_uname().sysname == 'Windows_NT'
-  local command = is_windows and 'where.exe node' or 'which node'
+  local result = vim.fn.exepath('node')
 
-  local result = vim.fn.system(command)
-  result = result:gsub('\r\n$', ''):gsub('\n$', '')
-
-  if vim.v.shell_error ~= 0 then
+  if result == '' then
     print('Error: Could not find Node.js path. ESLint server will use default path.')
     return nil
   end
